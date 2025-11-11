5ecf4f67e1e9815000c39474a01290868a95741b
shige15 <gabrielalfarezf15@gmail.com>
2025-11-11 17:32:56 +0700

    fix image

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -14,7 +14,7 @@ export default function LandingPage({ onNext }) {
       {/* Hero Image */}
       <div className="mt-10 flex justify-center">
         <img
-          src="/public/chayra-landings.png"
+          src="../Assets/chayra-landings.png"
           alt="Birthday Celebration"
           className="w-40 h-40 md:w-56 md:h-56 object-cover rounded-full drop-shadow-lg border-4 border-pink-200 animate-bounce"
         />
